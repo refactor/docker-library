682d0d1ce85e698fb89ab7ddf2dc322e261dcf39
wu lei <wulei@digitalearth.cn>
2015-06-29 18:32:09 +0800

    refactored directory

--- init.sh
+++
@@ -1,9 +0,0 @@
-mkdir tars
-cd tars
-wget -c ftp://ftp.gmplib.org/pub/gmp-5.0.2/gmp-5.0.2.tar.bz2
-wget -c http://www.mpfr.org/mpfr-current/mpfr-3.1.3.tar.gz
-wget -c http://www.multiprecision.org/mpC/download/mpc-0.9.tar.gz
-wget -c http://ftpmirror.gnu.org/binutils/binutils-2.21.1.tar.bz2
-wget -c http://ftpmirror.gnu.org/gcc/gcc-4.6.4/gcc-core-4.6.4.tar.bz2
-wget -c http://ftpmirror.gnu.org/gdb/gdb-7.3.1.tar.bz2
-cd ..
